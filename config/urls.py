7024ce083f50ffe33344382343cb0963248762bc
andres <hasfuraa@gmail.com>
2026-02-01 21:30:07 -0800

    Serve media explicitly in production

--- config/urls.py
+++ config/urls.py
@@ -17,7 +17,8 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.static import serve as static_serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,3 +27,6 @@ urlpatterns = [
 
 # Serve media in dev and small MVP deployments (Render free tier).
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', static_serve, {'document_root': settings.MEDIA_ROOT}),
+]
